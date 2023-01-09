bbd415cc7a7f7d4817783d0aae73c681651890c8
bilaljanir <bilaljanir@gmail.com>
2023-01-09 11:40:48 +0100

    add maven project structure

---
+++ src/main/java/jt/oopnext/users/Address.java
@@ -0,0 +1,14 @@
+package jt.oopnext.users;
+
+public class Address {
+
+    private int npa;
+
+    private int street;
+
+    private int Number;
+
+    private int city;
+
+
+}
--- src/main/java/jt/oopnext/users/User.java
+++ src/main/java/jt/oopnext/users/User.java
@@ -1,4 +1,12 @@
 package jt.oopnext.users;
 
 public class User {
+
+    private String email;
+
+    private String birthDate;
+
+    private String firstName;
+    private String lastname;
+    private Address address;
 }
